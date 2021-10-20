3625edf9f5d134b6c07cd32155c177459f504121
Evan BOISSONNOT <evan@boissonnot.fr>
2021-10-20 16:12:21 +0200

    feat(city): default form to add

--- SuiviWookies/SuiviWookies.Web.Ui/Controllers/CityController.cs
+++ SuiviWookies/SuiviWookies.Web.Ui/Controllers/CityController.cs
@@ -5,6 +5,7 @@ using SuiviWookies.Core.Services;
 using SuiviWookies.Web.Ui.Models;
 using System;
 using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
 using System.Linq;
 using System.Threading.Tasks;
 
@@ -33,6 +34,44 @@ namespace SuiviWookies.Web.Ui.Controllers
 
             return View(model);
         }
+
+        [HttpGet]
+        public IActionResult Add()
+        {
+            return this.View();
+        }
+
+        [HttpPost]
+        // public IActionResult Add(string gps, string label)
+        //public IActionResult Add(City city)
+        public IActionResult Add(TestOne test)
+        {
+            string newGps = this.Request.Form["gps"];
+
+            return this.View();
+        }
+
+        public IActionResult Update(City city)
+        {
+            return this.View();
+        }
         #endregion
+    }
+
+    public class TestOne
+    {
+        public string Label { get; set; }
+        public int Gps { get; set; }
+
+        [EmailAddress]
+        public string Email { get; set; }
+
+        public Child OneChild { get; set; }
+    }
+
+    public class Child
+    {   
+        public string Label { get; set; }
+
     }
 }
